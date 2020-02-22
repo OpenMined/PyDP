f8c13ee42478ff268cf51e28126522129ab27065
Chinmay Shah <chinmayshah3899@gmail.com>
2020-02-23 03:02:10 +0530

    added python2/3 check-support

--- build_PyDP.sh
+++ build_PyDP.sh
@@ -7,6 +7,7 @@ while (( "$#" )); do
         FARG=$2
         echo "-f option passed"
         shift 2
+        sed -i '160i\ \ \ \ python_bin_path=repository_ctx.which("python3")\n\ \ \ \ if\ python_bin_path != None:\n\ \ \ \ \ \ \ \ return\ str(python_bin_path)' ./third_party/pybind11_bazel/python_configure.bzl
         sed -i '18s/py/third_party\/pybind11_bazel\/py/' ./third_party/pybind11_bazel/python_configure.bzl
         sed -i '12iPYBIND11_BAZEL_DIR = "//third_party/pybind11_bazel"' ./third_party/pybind11_bazel/python_configure.bzl
         break
@@ -32,4 +33,3 @@ eval set -- "$PARAMS"
 bazel build src/python:bindings_test  --verbose_failures
 rm -f pydp.so
 cp -f ./bazel-bin/src/bindings/pydp.so .
-python test.py

56cf870127119833670006fcba13c76890b309df
NileshPant1999 <nileshpant6969@gmail.com>
2020-03-15 18:12:52 +0530

    Added Documentation for the code

--- examples/carrots.py
+++ examples/carrots.py
@@ -1,29 +1,29 @@
 # important to note that python's float type has the same precision as the C++ double
 
-import pydp as dp # our privacy library
+import pydp as dp    # our privacy library
 import pandas as pd
 
-class CarrotReporter:
+class CarrotReporter:  # Creating a class ClassReporter
 
     data_filename: str
     epsilon: float
 
-    def __init__(self, data_filename, epsilon):
+    def __init__(self, data_filename, epsilon): #Function to read the csv file and creating a dataframe
         self.data_filename = data_filename
         self.epsilon=epsilon
         self._epsilon=epsilon
         self._df = pd.read_csv(self.data_filename, sep=',', names=['animal', 'carrots_eaten'])
 
-    def sum_carrots(self) -> int: 
+    def sum_carrots(self) -> int:  # Function to return total number of carrots in dataset.
         return self._df.sum()[1]
 
-    def mean_carrots(self) -> float:
+    def mean_carrots(self) -> float: # Function to return mean of the carrots in the dataset.
         return self._df.mean()[0]
 
-    def count_above(self, limit: int) -> int:
+    def count_above(self, limit: int) -> int:  # Function to calculate total number of carrots above a particular row.
         return self._df[self._df.carrots_eaten>limit].count()[0]
 
-    def max(self) -> int:
+    def max(self) -> int:     # Function to calculate maximum number of carrots in the column.
         return self._df.max()[1]
 
     def privacy_budget(self) -> float:

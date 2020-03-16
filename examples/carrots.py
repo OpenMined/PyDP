2760bae71de0352ca66f7cb70fad05f881f46bac
NileshPant1999 <nileshpant6969@gmail.com>
2020-03-16 13:58:34 +0530

    Changes as ask by the reviewer

--- examples/carrots.py
+++ examples/carrots.py
@@ -1,29 +1,36 @@
 # important to note that python's float type has the same precision as the C++ double
 
-import pydp as dp    # our privacy library
+# our privacy library
+import pydp as dp
 import pandas as pd
 
-class CarrotReporter:  # Creating a class ClassReporter
+ # Creating a class ClassReporter
+class CarrotReporter:
 
     data_filename: str
     epsilon: float
 
-    def __init__(self, data_filename, epsilon): #Function to read the csv file and creating a dataframe
+    #Function to read the csv file and creating a dataframe
+    def __init__(self, data_filename, epsilon):
         self.data_filename = data_filename
         self.epsilon=epsilon
         self._epsilon=epsilon
         self._df = pd.read_csv(self.data_filename, sep=',', names=['animal', 'carrots_eaten'])
 
-    def sum_carrots(self) -> int:  # Function to return total number of carrots in dataset.
+    # Function to return total number of carrots in dataset.
+    def sum_carrots(self) -> int:
         return self._df.sum()[1]
 
-    def mean_carrots(self) -> float: # Function to return mean of the carrots in the dataset.
+    # Function to return mean of the carrots in the dataset.
+    def mean_carrots(self) -> float:
         return self._df.mean()[0]
 
-    def count_above(self, limit: int) -> int:  # Function to calculate total number of carrots above a particular row.
+    # Function to calculate total number of carrots above a particular row.
+    def count_above(self, limit: int) -> int:
         return self._df[self._df.carrots_eaten>limit].count()[0]
 
-    def max(self) -> int:     # Function to calculate maximum number of carrots in the column.
+     # Function to calculate maximum number of carrots in the column.
+    def max(self) -> int:
         return self._df.max()[1]
 
     def privacy_budget(self) -> float:

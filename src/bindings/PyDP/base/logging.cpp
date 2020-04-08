4a4959c8d74f006ab354b4ed11fde54433798489
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-08 19:36:28 +0100

    Update style of current C++ code

--- src/bindings/PyDP/base/logging.cpp
+++ src/bindings/PyDP/base/logging.cpp
@@ -4,18 +4,18 @@
 
 #include "pybind11/pybind11.h"
 
-#include "differential_privacy/base/logging.h" // the header file associated with logging.cc
-#include "../pydp_lib/casting.hpp" // our caster helper library
+#include "../pydp_lib/casting.hpp"  // our caster helper library
+#include "differential_privacy/base/logging.h"  // the header file associated with logging.cc
 
 namespace py = pybind11;
 namespace dpbase = differential_privacy::base;
 
-
 void init_base_logging(py::module &m) {
+  auto msub = m.def_submodule("logging");
 
-    auto msub = m.def_submodule("logging");
-
-    msub.def("get_log_directory", &dpbase::get_log_directory, "Get the current logging directory");
-    msub.def("get_vlog_level", &dpbase::get_vlog_level, "Get the current logging verbosity level");
-    msub.def("init_logging", &dpbase::InitLogging, "Begin logging");
+  msub.def("get_log_directory", &dpbase::get_log_directory,
+           "Get the current logging directory");
+  msub.def("get_vlog_level", &dpbase::get_vlog_level,
+           "Get the current logging verbosity level");
+  msub.def("init_logging", &dpbase::InitLogging, "Begin logging");
 }
--- src/bindings/PyDP/pydp_lib/casting.hpp
+++ src/bindings/PyDP/pydp_lib/casting.hpp
@@ -1,18 +1,17 @@
 // Convenient place to store type casters that are used through out project
 // This teaches pybind11 to cast types provided in the absl library
 
-
-#include "pybind11/stl.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
+#include "pybind11/stl.h"
 
 namespace pybind11 {
-    namespace detail
-    {
-        template <>
-        struct type_caster<absl::string_view> : string_caster<absl::string_view, true> {};
+namespace detail {
+template <>
+struct type_caster<absl::string_view> : string_caster<absl::string_view, true> {
+};
 
-        template <typename T>
-        struct type_caster<absl::optional<T>> : optional_caster<absl::optional<T>> {};
-    }   
-}
+template <typename T>
+struct type_caster<absl::optional<T>> : optional_caster<absl::optional<T>> {};
+}  // namespace detail
+}  // namespace pybind11

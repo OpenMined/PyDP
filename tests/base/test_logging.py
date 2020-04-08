325931d6067f2fd17f174e834dccf703a4a75bd9
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-08 19:08:53 +0100

    Update style of current Python code

--- tests/base/test_logging.py
+++ tests/base/test_logging.py
@@ -2,33 +2,37 @@ import pytest
 
 import pydp as pd
 
+
 class TestLogging():
     def test_logging(self):
         directory = 'tests'
         vlog_level = 3
-        l = pd.logging.init_logging(directory, 'log_test', vlog_level)
-        assert l == True
+        success = pd.logging.init_logging(directory, 'log_test', vlog_level)
+        assert success is True
 
     def test_logging1(self):
         directory = ''
         vlog_level = 3
-        l = pd.logging.init_logging(directory, 'log_test.txt', vlog_level)
-        assert l == False
+        success = pd.logging.init_logging(
+            directory, 'log_test.txt', vlog_level)
+        assert success is False
 
     def test_logging2(self):
         directory = 'tests'
         vlog_level = 3
-        l = pd.logging.init_logging(directory, '', vlog_level)
-        assert l == False
+        success = pd.logging.init_logging(directory, '', vlog_level)
+        assert success is False
 
     def test_vlog_level(self):
         directory = 'tests'
         vlog_level = 1
-        l = pd.logging.init_logging(directory, 'log_test.txt', vlog_level)
+        success = pd.logging.init_logging(
+            directory, 'log_test.txt', vlog_level)
         assert pd.logging.get_vlog_level() == vlog_level
-    
+
     def test_vlog_directory(self):
         directory = 'tests'
         vlog_level = 1
-        l = pd.logging.init_logging(directory, 'log_test.txt', vlog_level)
+        success = pd.logging.init_logging(
+            directory, 'log_test.txt', vlog_level)
         assert pd.logging.get_log_directory() == 'tests/'

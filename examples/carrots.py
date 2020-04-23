36bd27b692c53173e1a4632948854bd2b95bf66c
Chinmay Shah <chinmayshah3899@gmail.com>
2020-04-24 04:43:25 +0530

    added private mean support in example

--- examples/carrots.py
+++ examples/carrots.py
@@ -5,6 +5,7 @@
 
 import pydp as dp    # our privacy library
 import pandas as pd
+import statistics as s
 
 # Creating a class ClassReporter
 
@@ -19,7 +20,7 @@ class CarrotReporter:
         self.data_filename = data_filename
         self.epsilon = epsilon
         self._epsilon = epsilon
-        self._df = pd.read_csv(self.data_filename, sep=',', names=[
+        self._df = pd.read_csv('animals_and_carrots.csv', sep=',', names=[
                                'animal', 'carrots_eaten'])
 
     # Function to return total number of carrots in dataset.
@@ -28,7 +29,7 @@ class CarrotReporter:
 
     # Function to return mean of the carrots in the dataset.
     def mean_carrots(self) -> float:
-        return self._df.mean()[0]
+        return s.mean(list(self._df['carrots_eaten']))
 
     # Function to calculate total number of carrots above a particular row.
     def count_above(self, limit: int) -> int:
@@ -42,7 +43,9 @@ class CarrotReporter:
         return self._privacy_budget
 
     def private_sum(self, privacy_budget: float) -> dp.StatusOrO: pass
-    def private_mean(self, privacy_budget: float) -> dp.StatusOrO: pass
+    def private_mean(self, privacy_budget: float) -> dp.StatusOrO:
+        x = dp.BoundedMean(privacy_budget)
+        return x.result(list(self._df['carrots_eaten']))
     def private_count_above(self, privacy_budget: float,
                             limit: int) -> dp.StatusOrO: pass
 
@@ -58,3 +61,4 @@ print("Mean:\t" + str(c.mean_carrots()))
 print("Sum:\t" + str(c.sum_carrots()))
 print("Above 70:\t" + str(c.count_above(70)))
 print("Max:\t" + str(c.max()))
+print("private mean:\t" + str(c.private_mean(1)))

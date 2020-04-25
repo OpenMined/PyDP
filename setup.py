f0c246867410b7be63febf6368a48543e0a15d84
Chinmay Shah <chinmayshah3899@gmail.com>
2020-04-25 13:20:43 +0530

    Documentation update (#110)
    * Documentation update for release
    
    * pep8 fix

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup_requirements = []
 test_requirements = []
 
 setup(
-    author="Ben Szymkow",
-    author_email='simcof@gmail.com',
+    author="Chinmay Shah",
+    author_email='chinmayshah3899@gmail.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',

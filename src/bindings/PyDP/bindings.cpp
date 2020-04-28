ec7e59aab904715288b1733c5d91fa1336dc3be0
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-28 21:52:33 +0100

    Add bindings for base/percentile

--- src/bindings/PyDP/bindings.cpp
+++ src/bindings/PyDP/bindings.cpp
@@ -7,6 +7,7 @@ namespace py = pybind11;
 // base
 void init_base_status(py::module &);
 void init_base_logging(py::module &);
+void init_base_percentile(py::module &);
 
 // algorithms
 void init_algorithms(py::module &);
@@ -22,6 +23,7 @@ PYBIND11_MODULE(pydp, m) {
 
   init_base_status(m);
   init_base_logging(m);
+  init_base_percentile(m);
 
   init_algorithms(m);
 

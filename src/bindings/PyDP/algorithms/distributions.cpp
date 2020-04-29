32c119a089d4a7b4fd1f488444021b004280b688
Chinmay Shah <chinmayshah3899@gmail.com>
2020-04-29 13:35:17 +0530

    file organization in bindings

--- src/bindings/PyDP/algorithms/distributions.cpp
+++ src/bindings/PyDP/algorithms/distributions.cpp
@@ -6,31 +6,31 @@
 
 using namespace std;
 namespace py = pybind11;
-namespace dpi = differential_privacy::internal;
+namespace dpinternal = differential_privacy::internal;
 
 void declareLaplaceDistribution(py::module& m) {
-  py::class_<dpi::LaplaceDistribution>(m, "LaplaceDistribution")
+  py::class_<dpinternal::LaplaceDistribution>(m, "LaplaceDistribution")
       .def(py::init<double>())
-      .def("get_uniform_double", &dpi::LaplaceDistribution::GetUniformDouble)
+      .def("get_uniform_double", &dpinternal::LaplaceDistribution::GetUniformDouble)
       .def("sample",
-           (double (dpi::LaplaceDistribution::*)()) & dpi::LaplaceDistribution::Sample)
-      .def("sample", (double (dpi::LaplaceDistribution::*)(double)) &
-                         dpi::LaplaceDistribution::Sample)
-      .def("get_diversity", &dpi::LaplaceDistribution::GetDiversity)
-      .def("cdf", &dpi::LaplaceDistribution::cdf);
+           (double (dpinternal::LaplaceDistribution::*)()) & dpinternal::LaplaceDistribution::Sample)
+      .def("sample", (double (dpinternal::LaplaceDistribution::*)(double)) &
+                         dpinternal::LaplaceDistribution::Sample)
+      .def("get_diversity", &dpinternal::LaplaceDistribution::GetDiversity)
+      .def("cdf", &dpinternal::LaplaceDistribution::cdf);
 }
 
 void declareGaussianDistribution(py::module& m) {
-  py::class_<dpi::GaussianDistribution>(m, "GaussianDistribution")
+  py::class_<dpinternal::GaussianDistribution>(m, "GaussianDistribution")
       .def(py::init<double>())
-      .def("sample", (double (dpi::GaussianDistribution::*)()) &
-                         dpi::GaussianDistribution::Sample)
-      .def("sample", (double (dpi::GaussianDistribution::*)(double)) &
-                         dpi::GaussianDistribution::Sample)
-      .def("stddev", &dpi::GaussianDistribution::Stddev);
+      .def("sample", (double (dpinternal::GaussianDistribution::*)()) &
+                         dpinternal::GaussianDistribution::Sample)
+      .def("sample", (double (dpinternal::GaussianDistribution::*)(double)) &
+                         dpinternal::GaussianDistribution::Sample)
+      .def("stddev", &dpinternal::GaussianDistribution::Stddev);
 }
 
-void init_distributions(py::module& m) {
+void init_algorithms_distributions(py::module& m) {
   declareLaplaceDistribution(m);
   declareGaussianDistribution(m);
 }

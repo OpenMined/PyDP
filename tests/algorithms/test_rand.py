6bc78c2d601e420c825c32394aa2ad05d31b25ae
Lucas Hagel <lucas.hagel@disroot.org>
2020-05-03 00:36:06 -0600

    formatting

--- tests/algorithms/test_rand.py
+++ tests/algorithms/test_rand.py
@@ -1,9 +1,12 @@
 import pytest
 import pydp as dp
 
+
 def test_rand_UniformDouble():
-	ud = dp.UniformDouble()
-	assert isinstance(ud,float)
+    ud = dp.UniformDouble()
+    assert isinstance(ud, float)
+
+
 def test_rand_Geometric():
-	gn = dp.Geometric()
-	assert isinstance(gn,int)
+    gn = dp.Geometric()
+    assert isinstance(gn, int)

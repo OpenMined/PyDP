ca7a65697d1b50d58d5857f5450701d5ec6e82ef
Chinmay Shah <chinmayshah3899@gmail.com>
2020-05-10 19:38:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/OpenMined/PyDP",
-    version="0.1.1",
+    version="0.1.11", # just testing
     zip_safe=False,
 )

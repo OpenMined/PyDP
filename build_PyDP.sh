e2bc1f3ba5b28c55f7290d19f964ec459512d3e5
Madhava Jay <me@madhavajay.com>
2020-05-31 10:38:13 +1000

    Fixed issue in build_PyDP.sh with find | xargs on linux

--- build_PyDP.sh
+++ build_PyDP.sh
@@ -2,5 +2,5 @@
 
 pipenv run pipenv install --dev --skip-lock
 pipenv run bazel build src/python:bindings_test  --verbose_failures
-find ./ -name pydp.so | xargs rm
+find ./ -name pydp.so -print0 | xargs -0 -I {} rm {}
 cp -f ./bazel-bin/src/bindings/pydp.so ./pydp

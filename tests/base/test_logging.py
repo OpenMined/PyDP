d63f3e2e4d0f5af832498128111a7acbb8e1bc97
Saksham Rastogi <40931412+codeboy5@users.noreply.github.com>
2020-06-01 01:21:33 +0530

    Fixed the issue with the Logger tests (#168)
    * fixed the issue with the logger tests
    
    * style fix
    
    * cpp style fix

--- tests/base/test_logging.py
+++ tests/base/test_logging.py
@@ -1,34 +1,34 @@
 import pytest
 import pydp as dp
 
-# TODO :- add a read_only_property to the `logging.cc` file.
-# class TestLogging:
-#     def test_logging(self):
-#         directory = "tests"
-#         vlog_level = 3
-#         success = dp.Logging(directory, "log_test", vlog_level)
-#         assert success is True
 
-#     def test_logging1(self):
-#         directory = ""
-#         vlog_level = 3
-#         success = dp.Logging(directory, "log_test.txt", vlog_level)
-#         assert success is False
+class TestLogging:
+    def test_logging(self):
+        directory = "tests"
+        vlog_level = 3
+        tl = dp.Logging(directory, "log_test", vlog_level)
+        assert tl.log_status is True
 
-#     def test_logging2(self):
-#         directory = "tests"
-#         vlog_level = 3
-#         success = dp.Logging(directory, "", vlog_level)
-#         assert success is False
+    def test_logging1(self):
+        directory = ""
+        vlog_level = 3
+        tl1 = dp.Logging(directory, "log_test.txt", vlog_level)
+        assert tl1.log_status is False
 
-#     def test_vlog_level(self):
-#         directory = "tests"
-#         vlog_level = 1
-#         success = dp.Logging(directory, "log_test.txt", vlog_level)
-#         assert dp.Logging.vlog_level == vlog_level
+    def test_logging2(self):
+        directory = "tests"
+        vlog_level = 3
+        tl2 = dp.Logging(directory, "", vlog_level)
+        assert tl2.log_status is False
 
-#     def test_vlog_directory(self):
-#         directory = "tests"
-#         vlog_level = 1
-#         success = dp.logging.init_logging(directory, "log_test.txt", vlog_level)
-#         assert dp.Logging.log_directory == "tests/"
+    def test_vlog_level(self):
+        directory = "tests"
+        vlog_level = 1
+        tvl = dp.Logging(directory, "log_test.txt", vlog_level)
+        assert tvl.vlog_level == vlog_level
+
+    def test_vlog_directory(self):
+        directory = "tests"
+        vlog_level = 1
+        tvd = dp.Logging(directory, "log_test.txt", vlog_level)
+        assert tvd.log_directory == "tests/"

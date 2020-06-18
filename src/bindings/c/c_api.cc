7b80933992a021e581bb56e3979c2752c4922085
Chinmay Shah <chinmayshah3899@gmail.com>
2020-06-19 04:12:27 +0530

    fixed cpp style

--- src/bindings/c/c_api.cc
+++ src/bindings/c/c_api.cc
@@ -27,26 +27,26 @@ double Result_BoundedMean(BoundedFunctionHelperObject* config, pybind11::list l)
   }
   base::StatusOr<std::unique_ptr<BoundedMean<double>>> mean_obj;
   if (has_bounds) {
-     mean_obj = BoundedMean<double>::Builder()
-               .SetEpsilon(config->epsilon)
-               .SetLower(config->lower)
-               .SetUpper(config->upper)
-               .Build();
+    mean_obj = BoundedMean<double>::Builder()
+                   .SetEpsilon(config->epsilon)
+                   .SetLower(config->lower)
+                   .SetUpper(config->upper)
+                   .Build();
   } else {
-    // TODO: a better solution to this is needed similar to ASSIGN_OR_RETURN but with a raised exeception
+    // TODO: a better solution to this is needed similar to ASSIGN_OR_RETURN but with a
+    // raised exeception
     mean_obj = BoundedMean<double>::Builder().SetEpsilon(config->epsilon).Build();
   }
-    
-  if (!mean_obj.ok()){
-     // TODO: custtom error?
+
+  if (!mean_obj.ok()) {
+    // TODO: custtom error?
     throw std::runtime_error(mean_obj.status().error_message());
   }
 
   base::StatusOr<Output> resultf = mean_obj.ValueOrDie()->Result(a.begin(), a.end());
-  if (resultf.ok()){
+  if (resultf.ok()) {
     return GetValue<double>(resultf.ValueOrDie());
-  }
-  else{
+  } else {
     throw std::runtime_error(resultf.status().error_message());
   }
 }
@@ -60,25 +60,25 @@ double Result_BoundedSum(BoundedFunctionHelperObject* config, pybind11::list l) 
   }
   base::StatusOr<std::unique_ptr<BoundedSum<double>>> sum_obj;
   if (has_bounds) {
-     sum_obj = BoundedSum<double>::Builder()
-               .SetEpsilon(config->epsilon)
-               .SetLower(config->lower)
-               .SetUpper(config->upper)
-               .Build();
+    sum_obj = BoundedSum<double>::Builder()
+                  .SetEpsilon(config->epsilon)
+                  .SetLower(config->lower)
+                  .SetUpper(config->upper)
+                  .Build();
   } else {
-    // TODO: a better solution to this is needed similar to ASSIGN_OR_RETURN but with a raised exeception
+    // TODO: a better solution to this is needed similar to ASSIGN_OR_RETURN but with a
+    // raised exeception
     sum_obj = BoundedSum<double>::Builder().SetEpsilon(config->epsilon).Build();
   }
-    
-  if (!sum_obj.ok()){
+
+  if (!sum_obj.ok()) {
     throw std::runtime_error(sum_obj.status().error_message());
   }
 
   base::StatusOr<Output> resultf = sum_obj.ValueOrDie()->Result(a.begin(), a.end());
-  if (resultf.ok()){
+  if (resultf.ok()) {
     return GetValue<double>(resultf.ValueOrDie());
-  }
-  else{
+  } else {
     throw std::runtime_error(resultf.status().error_message());
   }
 }
@@ -92,25 +92,26 @@ double Result_BoundedStandardDeviation(BoundedFunctionHelperObject* config,
   }
   base::StatusOr<std::unique_ptr<BoundedStandardDeviation<double>>> sd_obj;
   if (has_bounds) {
-     sd_obj = BoundedStandardDeviation<double>::Builder()
-               .SetEpsilon(config->epsilon)
-               .SetLower(config->lower)
-               .SetUpper(config->upper)
-               .Build();
+    sd_obj = BoundedStandardDeviation<double>::Builder()
+                 .SetEpsilon(config->epsilon)
+                 .SetLower(config->lower)
+                 .SetUpper(config->upper)
+                 .Build();
   } else {
-    // TODO: a better solution to this is needed similar to ASSIGN_OR_RETURN but with a raised exeception
-    sd_obj = BoundedStandardDeviation<double>::Builder().SetEpsilon(config->epsilon).Build();
+    // TODO: a better solution to this is needed similar to ASSIGN_OR_RETURN but with a
+    // raised exeception
+    sd_obj =
+        BoundedStandardDeviation<double>::Builder().SetEpsilon(config->epsilon).Build();
   }
-    
-  if (!sd_obj.ok()){
+
+  if (!sd_obj.ok()) {
     throw std::runtime_error(sd_obj.status().error_message());
   }
 
   base::StatusOr<Output> resultf = sd_obj.ValueOrDie()->Result(a.begin(), a.end());
-  if (resultf.ok()){
+  if (resultf.ok()) {
     return GetValue<double>(resultf.ValueOrDie());
-  }
-  else{
+  } else {
     throw std::runtime_error(resultf.status().error_message());
   }
 }
@@ -123,24 +124,24 @@ double Result_BoundedVariance(BoundedFunctionHelperObject* config, pybind11::lis
   }
   base::StatusOr<std::unique_ptr<BoundedVariance<double>>> variance_obj;
   if (has_bounds) {
-     variance_obj = BoundedVariance<double>::Builder()
-               .SetEpsilon(config->epsilon)
-               .SetLower(config->lower)
-               .SetUpper(config->upper)
-               .Build();
+    variance_obj = BoundedVariance<double>::Builder()
+                       .SetEpsilon(config->epsilon)
+                       .SetLower(config->lower)
+                       .SetUpper(config->upper)
+                       .Build();
   } else {
-    variance_obj = BoundedVariance<double>::Builder().SetEpsilon(config->epsilon).Build();
+    variance_obj =
+        BoundedVariance<double>::Builder().SetEpsilon(config->epsilon).Build();
   }
-    
-  if (!variance_obj.ok()){
+
+  if (!variance_obj.ok()) {
     throw std::runtime_error(variance_obj.status().error_message());
-  }
-  else{
-    base::StatusOr<Output> resultf = variance_obj.ValueOrDie()->Result(a.begin(), a.end());
-    if (resultf.ok()){
+  } else {
+    base::StatusOr<Output> resultf =
+        variance_obj.ValueOrDie()->Result(a.begin(), a.end());
+    if (resultf.ok()) {
       return GetValue<double>(resultf.ValueOrDie());
-    }
-    else{
+    } else {
       throw std::runtime_error(resultf.status().error_message());
     }
   }
@@ -155,27 +156,23 @@ int64_t Result_Max(BoundedFunctionHelperObject* config, pybind11::list l,
   base::StatusOr<std::unique_ptr<continuous::Max<int64_t>>> max;
   if (has_bounds) {
     max = continuous::Max<int64_t>::Builder()
-                 .SetEpsilon(config->epsilon)
-                 .SetLower(config->lower)
-                 .SetUpper(config->upper)
-                 .Build();
+              .SetEpsilon(config->epsilon)
+              .SetLower(config->lower)
+              .SetUpper(config->upper)
+              .Build();
   } else {
-    max = continuous::Max<int64_t>::Builder()
-                 .SetEpsilon(config->epsilon)
-                 .Build();
+    max = continuous::Max<int64_t>::Builder().SetEpsilon(config->epsilon).Build();
   }
-  if (!max.ok()){
+  if (!max.ok()) {
     throw std::runtime_error(max.status().error_message());
-  }
-  else{
+  } else {
     for (auto i : l) {
       max.ValueOrDie()->AddEntry(i.cast<int64_t>());
     }
     base::StatusOr<Output> resultf = max.ValueOrDie()->PartialResult(privacy_budget);
-    if (resultf.ok()){
+    if (resultf.ok()) {
       return GetValue<int64_t>(resultf.ValueOrDie());
-    }
-    else{
+    } else {
       throw std::runtime_error(resultf.status().error_message());
     }
   }
@@ -188,27 +185,23 @@ int64_t Result_Min(BoundedFunctionHelperObject* config, pybind11::list l,
   base::StatusOr<std::unique_ptr<continuous::Min<int64_t>>> min;
   if (has_bounds) {
     min = continuous::Min<int64_t>::Builder()
-                 .SetEpsilon(config->epsilon)
-                 .SetLower(config->lower)
-                 .SetUpper(config->upper)
-                 .Build();
+              .SetEpsilon(config->epsilon)
+              .SetLower(config->lower)
+              .SetUpper(config->upper)
+              .Build();
   } else {
-    min = continuous::Min<int64_t>::Builder()
-                 .SetEpsilon(config->epsilon)
-                 .Build();
+    min = continuous::Min<int64_t>::Builder().SetEpsilon(config->epsilon).Build();
   }
-  if (!min.ok()){
+  if (!min.ok()) {
     throw std::runtime_error(min.status().error_message());
-  }
-  else{
+  } else {
     for (auto i : l) {
       min.ValueOrDie()->AddEntry(i.cast<int64_t>());
     }
     base::StatusOr<Output> resultf = min.ValueOrDie()->PartialResult(privacy_budget);
-    if (resultf.ok()){
+    if (resultf.ok()) {
       return GetValue<int64_t>(resultf.ValueOrDie());
-    }
-    else{
+    } else {
       throw std::runtime_error(resultf.status().error_message());
     }
   }
@@ -226,22 +219,18 @@ int64_t Result_Median(BoundedFunctionHelperObject* config, pybind11::list l,
                  .SetUpper(config->upper)
                  .Build();
   } else {
-    median = continuous::Median<int64_t>::Builder()
-                 .SetEpsilon(config->epsilon)
-                 .Build();
+    median = continuous::Median<int64_t>::Builder().SetEpsilon(config->epsilon).Build();
   }
-  if (!median.ok()){
+  if (!median.ok()) {
     throw std::runtime_error(median.status().error_message());
-  }
-  else{
+  } else {
     for (auto i : l) {
       median.ValueOrDie()->AddEntry(i.cast<int64_t>());
     }
     base::StatusOr<Output> resultf = median.ValueOrDie()->PartialResult(privacy_budget);
-    if (resultf.ok()){
+    if (resultf.ok()) {
       return GetValue<int64_t>(resultf.ValueOrDie());
-    }
-    else{
+    } else {
       throw std::runtime_error(resultf.status().error_message());
     }
   }
@@ -291,8 +280,6 @@ BoundedFunctionHelperObject* NewBoundedFunctionObject1(double epsilon) {
 void DeleteBoundedFunctionObject(BoundedFunctionHelperObject* config) {
   delete config;
 };
-
-
 
 }  // end namespace differential_privacy
 }  // end extern "C"

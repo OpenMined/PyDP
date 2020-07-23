2fda9ec100dbc9c9dd9688725f944b3a810958df
benjamindev <benjamin.decharmoy@gmail.com>
2020-07-23 16:26:50 +0000

    updated to latest and greatest upstream 5735d7c. Added the GeometricDistribution and fixed LaplaceDistribution. Updated upstream sha.

--- src/bindings/PyDP/algorithms/distributions.cpp
+++ src/bindings/PyDP/algorithms/distributions.cpp
@@ -8,31 +8,70 @@ using namespace std;
 namespace py = pybind11;
 namespace dpi = differential_privacy::internal;
 
-void declareLaplaceDistribution(py::module& m) {
+void declareLaplaceDistribution(py::module &m) {
   py::class_<dpi::LaplaceDistribution> laplace_dist(m, "LaplaceDistribution");
   laplace_dist.attr("__module__") = "pydp";
-  laplace_dist.def(py::init<double, double>())
-      .def("get_uniform_double", &dpi::LaplaceDistribution::GetUniformDouble)
-      .def("sample",
-           (double (dpi::LaplaceDistribution::*)()) & dpi::LaplaceDistribution::Sample)
-      .def("sample", (double (dpi::LaplaceDistribution::*)(double)) &
-                         dpi::LaplaceDistribution::Sample)
-      .def("get_diversity", &dpi::LaplaceDistribution::GetDiversity);
-  // .def("cdf", &dpi::LaplaceDistribution::cdf);
+  laplace_dist
+      .def(py::init<double, double>(), py::arg("epsilon") = 0., py::arg("sensitivity"))
+      .def("get_uniform_double", &dpi::LaplaceDistribution::GetUniformDouble,
+           R"pbdoc(Returns a uniform random integer of in range [0, 2^53).)pbdoc")
+      .def("sample", py::overload_cast<double>(&dpi::LaplaceDistribution::Sample),
+           py::arg("scale") = 1.0,
+           R"pbdoc(
+                    Samples the Laplacian distribution Laplace(u, scale*b).
+                    
+                    Parameters
+                    ----------
+                    scale
+                         A factor to scale b.
+               )pbdoc");
+  laplace_dist.def("get_diversity", &dpi::LaplaceDistribution::GetDiversity,
+                   R"pbdoc(
+               Returns the parameter defining this distribution, often labeled b.
+               
+          )pbdoc");
+  laplace_dist.attr("__doc__") = "Draws samples from the Laplacian distribution.";
 }
 
-void declareGaussianDistribution(py::module& m) {
+void declareGaussianDistribution(py::module &m) {
   py::class_<dpi::GaussianDistribution> gauss_dist(m, "GaussianDistribution");
   gauss_dist.attr("__module__") = "pydp";
   gauss_dist.def(py::init<double>())
-      .def("sample", (double (dpi::GaussianDistribution::*)()) &
-                         dpi::GaussianDistribution::Sample)
-      .def("sample", (double (dpi::GaussianDistribution::*)(double)) &
-                         dpi::GaussianDistribution::Sample)
-      .def("stddev", &dpi::GaussianDistribution::Stddev);
+      .def("sample", py::overload_cast<double>(&dpi::GaussianDistribution::Sample),
+           py::arg("scale") = 1.0,
+           R"pbdoc(Samples the Gaussian with distribution Gauss(scale*stddev).
+                    Parameters
+                    ----------
+                    scale
+                         A factor to scale stddev.    
+             )pbdoc")
+      .def("stddev", &dpi::GaussianDistribution::Stddev, R"pbdoc(Returns stddev)pbdoc");
 }
 
-void init_algorithms_distributions(py::module& m) {
+void declareGeometricDistribution(py::module &m) {
+  py::class_<dpi::GeometricDistribution> geometric_dist(m, "GeometricDistribution");
+  geometric_dist.attr("__module__") = "pydp";
+  geometric_dist
+      .def(py::init<double>(), py::arg("lambda_"),
+           "Constructs a GeometricDistribution, p = 1 - e^-lambda.")
+      .def("get_uniform_double", &dpi::GeometricDistribution::GetUniformDouble)
+
+      .def("sample", py::overload_cast<double>(&dpi::GeometricDistribution::Sample),
+           py::arg("scale") = 1.0, "Returns a sample from p = 1 - e^-(lambda/scale).")
+
+      .def("lambda", &dpi::GeometricDistribution::Lambda,
+           R"pbdoc(Returns lambda. Where p = 1 - e^-lambda)pbdoc");
+  geometric_dist.attr("__doc__") =
+      R"pbdoc(Draws samples from the geometric distribution of probability
+          \math{p = 1 - e^{-\lambda}}, i.e. the number of bernoulli trial failures before the
+          first success where the success probability is as defined above. lambda must
+          be positive. If the result would be higher than the maximum int64_t, returns
+          the maximum int64_t, which means that users should be careful around the edges
+          of their distribution)pbdoc";
+}
+
+void init_algorithms_distributions(py::module &m) {
   declareLaplaceDistribution(m);
   declareGaussianDistribution(m);
+  declareGeometricDistribution(m);
 }
--- tests/algorithms/test_distributions.py
+++ tests/algorithms/test_distributions.py
@@ -1,34 +1,69 @@
 import pytest
 import pydp as dp
 import math
+from typing import List
+from itertools import accumulate
+import math
+k_num_samples = 10000000
+k_num_geometric_samples = 1000000
+k_gaussian_samples = 1000000
+k_one_over_log2 = 1.44269504089
 
-kOneOverLog2 = 1.44269504089
+def skew(samples:List[float], mu:float, sigma:float):
+    """Unfortunately this is implemented in third_party/differential-privacy/cc/algorithms/distributions_test.cc
+       and we don't want to pull the test files in. I'm assuming it'll be moved to
+       third_party/differential-privacy/cc/algorithms/util.h If they (upstream) move it we can use it.
+       Until then this should suffice. #FIXME: when possible we can fix this.  
+    """
+    skew = list(accumulate(samples, lambda lhs, rhs: lhs + (rhs - mu) * (rhs - mu) * (rhs - mu)))[-1]
+    return skew / (len(samples) * sigma * sigma * sigma)
+    
+def kurtosis(samples:List[float], mu:float, var:float):
+    """Unfortunately this is implemented in third_party/differential-privacy/cc/algorithms/distributions_test.cc
+       and we don't want to pull the test files in. I'm assuming it'll be moved to
+       third_party/differential-privacy/cc/algorithms/util.h If they (upstream) move it we can use it.
+       Until then this should suffice. #FIXME: when possible we can fix this.  
+    """
+    kurt = list(accumulate(samples, lambda lhs, rhs: lhs + ((rhs - mu) * (rhs - mu))**2))[-1]
+    n = len(samples)
+    kurt = (n + 1) * kurt / (n * var * var)
+    kurt -= 3 * (n - 1)
+    kurt *= (n - 1) / (n - 2) / (n - 3)
+    return kurt
 
+# From what I understand @openmined/dp-research are going to look at validating correctness
+# Until then we can use this to assert on floating point numbers.
+# FIXME: When possible we should add 'correctness' tests.
+expect_near = lambda expected, actual, tol: (expected + tol >= actual and  expected - tol <= actual)
 
-# legacy tests
-# no new tests exists for these that can be tested
-# class TestLaplaceDistribution:
-# def test_diversity_getter(self):
-#     stddev = kOneOverLog2
-#     dist = dp.LaplaceDistribution(stddev, 1.0)
-#     assert dist.get_diversity() == stddev
+class TestLaplaceDistribution:
+    def test_diversity_getter(self):
+        sensitivity,  epsilon = 1.0, 22.0
+        dist = dp.LaplaceDistribution(epsilon=epsilon, sensitivity=sensitivity)
+        assert dist.get_diversity() == sensitivity / epsilon
 
-# def test_cdf(self):
-#     assert dp.LaplaceDistribution.cdf(5, 0) == 0.5
-#     assert dp.LaplaceDistribution.cdf(1, -1) == 0.5 * math.exp(-1)
-#     assert dp.LaplaceDistribution.cdf(1, 1) == 1 - 0.5 * math.exp(-1)
+    def test_check_statistics_for_geo_unit_values(self):
 
+        ld = dp.LaplaceDistribution(epsilon=1.0, sensitivity=1.0)
+        samples = [ld.sample(scale=1.0) for _ in range(k_num_geometric_samples)]
+        mean = dp.util.mean(samples)
+        var = dp.util.variance(samples)
+        
+        assert expect_near(0.0, mean, 0.01)
+        assert expect_near(2.0, var, 0.1)
+        assert expect_near(0.0, skew(samples, mean, math.sqrt(var)), 0.1)
+        assert expect_near(3.0, kurtosis(samples, mean, var), 0.1)
 
 class TestGaussianDistribution:
     def test_standard_deviation_getter(self):
-        stddev = kOneOverLog2
+        stddev = k_one_over_log2
         dist = dp.GaussianDistribution(stddev)
         assert dist.stddev() == stddev
 
 
 class TestLaplaceDistributionDatatypes:
     def test_LaplaceDistributionTypes(self):
-        ld = dp.LaplaceDistribution(2.0, 1.0)
+        ld = dp.LaplaceDistribution(epsilon=1.0, sensitivity=1.0)
         assert isinstance(ld, dp.LaplaceDistribution)
 
         sud = ld.get_uniform_double()
@@ -55,6 +90,34 @@ class TestGaussianDistributionDataTypes:
         assert isinstance(gds1, float)
         gdstd = gd.stddev()
         assert isinstance(gdstd, float)
+
+from collections import Counter
+class TestGeometricDistribution:
+    def test_ratios(self):
+        p=1e-2
+        dist = dp.GeometricDistribution(lambda_=-1.0*math.log(1-p))
+        samples  = [dist.sample() for _ in range(k_num_geometric_samples)]
+        counts = list(Counter([s for s in samples if s < 51]).values())
+        ratios = [c_j/c_i for c_i, c_j in zip(counts[:-1], counts[1:])]
+        assert expect_near(p, dp.util.mean(ratios), p / 1e-2)
+
+# TEST(GeometricDistributionTest, Ratios) {
+#   double p = 1e-2;
+#   GeometricDistribution dist(-1.0 * std::log(1.0 - p));
+# #   std::vector<int64_t> counts(51, 0);
+# #   for (int i = 0; i < kNumGeometricSamples; ++i) {
+# #     int64_t sample = dist.Sample();
+# #     if (sample < counts.size()) {
+# #       ++counts[sample];
+# #     }
+# #   }
+#   std::vector<double> ratios;
+#   for (int i = 0; i < counts.size() - 1; ++i) {
+#     ratios.push_back(static_cast<double>(counts[i + 1]) /
+#                      static_cast<double>(counts[i]));
+#   }
+#   EXPECT_NEAR(p, Mean(ratios), p / 1e-2);
+# }
 
 
 # TODO: port the following tests

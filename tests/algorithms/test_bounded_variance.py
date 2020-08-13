7d53895ebfe74af4457e1038781274375facff77
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-08-13 19:22:32 +0100

    Update bounded algorithm tests

--- tests/algorithms/test_bounded_variance.py
+++ tests/algorithms/test_bounded_variance.py
@@ -1,4 +1,4 @@
-import pydp as dp
+from pydp.algorithms.laplacian import BoundedVariance
 
 
 class TestBoundedVariance:
@@ -6,7 +6,7 @@ class TestBoundedVariance:
         example_data = [1, 5, 7, 9, 13]
         epsilon = 1.0
         lower_bound, upper_bound = 0, 16
-        bv = dp.BoundedVariance(epsilon, lower_bound, upper_bound)
+        bv = BoundedVariance(epsilon, lower_bound, upper_bound, dtype="float")
         result = bv.result(example_data)
         assert type(result) is float and result >= 0
         # Popoviciu's inequality on variances:

ebc29d1ffd85f58ce5b385243c620aa939bf8716
Chinmay Shah <chinmayshah3899@gmail.com>
2020-08-18 10:45:48 +0530

    changed release version to 0.1.6

--- pydp/__init__.py
+++ pydp/__init__.py
@@ -1,4 +1,4 @@
 from pydp import algorithms, distributions, util
 
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     url="https://github.com/OpenMined/PyDP",
-    version="0.1.5",
+    version="0.1.6",
     zip_safe=False,
 )

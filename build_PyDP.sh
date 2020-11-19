be503196bfa4ff57ec949d3ff96fc1859d56e77b
siddhesh1598 <siddhesh1598@gmail.com>
2020-11-19 13:40:40 +0530

    moved pydp folver under src

--- build_PyDP.sh
+++ build_PyDP.sh
@@ -3,4 +3,4 @@
 poetry install
 bazel coverage src/python:bindings_test --config Linux src/python:bindings_test  --verbose_failures
 find ./ -name _pydp.so -print0 | xargs -0 -I {} rm -r {}
-cp -f ./bazel-bin/src/bindings/_pydp.so ./pydp
+cp -f ./bazel-bin/src/bindings/_pydp.so ./src/pydp

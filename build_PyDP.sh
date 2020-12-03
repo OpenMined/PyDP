56112b1dd852309697b007794396466a42d7ca21
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-12-03 16:36:34 +0000

    Explicitly tell pybind which python to use

--- build_PyDP.sh
+++ build_PyDP.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# Setup poetry env
 poetry install
-bazel coverage src/python:bindings_test --config Linux src/python:bindings_test  --verbose_failures
+
+# Search specific python bin and lib folders to compile against the poetry env
+PYTHONHOME=$(poetry run which python)
+PYTHONPATH=$(poetry run python -c 'import sys; print([x for x in sys.path if "site-packages" in x][0]);')
+
+bazel coverage src/python:bindings_test \
+  --config Linux src/python:bindings_test \
+  --verbose_failures \
+  --action_env=PYTHON_BIN_PATH=$PYTHONHOME \
+  --action_env=PYTHON_LIB_PATH=$PYTHONPATH
+
+# Delete the previously compiled package and copy the new one
 find ./ -name _pydp.so -print0 | xargs -0 -I {} rm -r {}
 cp -f ./bazel-bin/src/bindings/_pydp.so ./src/pydp

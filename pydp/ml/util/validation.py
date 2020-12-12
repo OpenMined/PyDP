afc3df48baf6af416a66e5632e841faaa5be2caf
grilhami <gilangrilhami@hotmail.co.uk>
2020-12-12 10:55:27 +0000

    Added function to check min max bounds in validation.py

--- pydp/ml/util/validation.py
+++ pydp/ml/util/validation.py
@@ -28,3 +28,72 @@ def check_epsilon_delta(epsilon, delta, allow_zero=False):
 
     if not allow_zero and epsilon + delta == 0:
         raise ValueError("Epsilon and Delta cannot both be zero")
+
+def check_bounds(bounds, shape=0, min_separation=0.0, dtype=float):
+    """Input validation for the ``bounds`` parameter.
+    Checks that ``bounds`` is composed of a list of tuples of the form (lower, upper), where lower <= upper and both
+    are numeric.  Also checks that ``bounds`` contains the appropriate number of dimensions, and that there is a
+    ``min_separation`` between the bounds.
+    Parameters
+    ----------
+    bounds : tuple
+        Tuple of bounds of the form (min, max). `min` and `max` can either be scalars or 1-dimensional arrays.
+    shape : int, default: 0
+        Number of dimensions to be expected in ``bounds``.
+    min_separation : float, default: 0.0
+        The minimum separation between `lower` and `upper` of each dimension.  This separation is enforced if not
+        already satisfied.
+    dtype : data-type, default: float
+        Data type of the returned bounds.
+    Returns
+    -------
+    bounds : tuple
+    """
+    if not isinstance(bounds, tuple):
+        raise TypeError("Bounds must be specified as a tuple of (min, max), got {}.".format(type(bounds)))
+    if not isinstance(shape, Integral):
+        raise TypeError("shape parameter must be integer-valued, got {}.".format(type(shape)))
+
+    lower, upper = bounds
+
+    if np.asarray(lower).size == 1 or np.asarray(upper).size == 1:
+        lower = np.ravel(lower).astype(dtype)
+        upper = np.ravel(upper).astype(dtype)
+    else:
+        lower = np.asarray(lower, dtype=dtype)
+        upper = np.asarray(upper, dtype=dtype)
+
+    if lower.shape != upper.shape:
+        raise ValueError("lower and upper bounds must be the same shape array")
+    if lower.ndim > 1:
+        raise ValueError("lower and upper bounds must be scalar or a 1-dimensional array")
+    if lower.size != shape and lower.size != 1:
+        raise ValueError("lower and upper bounds must have {} element(s), got {}.".format(shape or 1, lower.size))
+
+    n_bounds = lower.shape[0]
+
+    for i in range(n_bounds):
+        _lower = lower[i]
+        _upper = upper[i]
+
+        if not isinstance(_lower, Real) or not isinstance(_upper, Real):
+            raise TypeError("Each bound must be numeric, got {} ({}) and {} ({}).".format(_lower, type(_lower),
+                                                                                          _upper, type(_upper)))
+
+        if _lower > _upper:
+            raise ValueError("For each bound, lower bound must be smaller than upper bound, got {}, {})".format(
+                lower, upper))
+
+        if _upper - _lower < min_separation:
+            mid = (_upper + _lower) / 2
+            lower[i] = mid - min_separation / 2
+            upper[i] = mid + min_separation / 2
+
+    if shape == 0:
+        return lower.item(), upper.item()
+
+    if n_bounds == 1:
+        lower = np.ones(shape, dtype=dtype) * lower.item()
+        upper = np.ones(shape, dtype=dtype) * upper.item()
+
+    return lower, upper

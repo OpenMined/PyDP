386acea052d3b228bd49f3433b8539ecd93fa1d0
grilhami <gilangrilhami@hotmail.co.uk>
2020-12-12 12:17:55 +0000

    Added subclass Budget for privacy budgets epsilon delta

--- pydp/ml/util/utils.py
+++ pydp/ml/util/utils.py
@@ -34,3 +34,45 @@ def copy_docstring(source):
             target.__doc__ = source.__doc__
         return target
     return copy_func
+
+class Budget(tuple):
+    """Custom tuple subclass for privacy budgets of the form (epsilon, delta).
+    The ``Budget`` class allows for correct comparison/ordering of privacy budget, ensuring that both epsilon and delta
+    satisfy the comparison (tuples are compared lexicographically).  Additionally, tuples are represented with added
+    verbosity, labelling epsilon and delta appropriately.
+    Examples
+    --------
+    >>> from pydp.ml.util.utils import Budget
+    >>> Budget(1, 0.5)
+    (epsilon=1, delta=0.5)
+    >>> Budget(2, 0) >= Budget(1, 0.5)
+    False
+    >>> (2, 0) >= (1, 0.5) # Tuples are compared with lexicographic ordering
+    True
+    """
+    def __new__(cls, epsilon, delta):
+        check_epsilon_delta(epsilon, delta, allow_zero=True)
+        return tuple.__new__(cls, (epsilon, delta))
+
+    def __gt__(self, other):
+        if self.__ge__(other) and not self.__eq__(other):
+            return True
+        return False
+
+    def __ge__(self, other):
+        if self[0] >= other[0] and self[1] >= other[1]:
+            return True
+        return False
+
+    def __lt__(self, other):
+        if self.__le__(other) and not self.__eq__(other):
+            return True
+        return False
+
+    def __le__(self, other):
+        if self[0] <= other[0] and self[1] <= other[1]:
+            return True
+        return False
+
+    def __repr__(self):
+        return "(epsilon=%r, delta=%r)" % self

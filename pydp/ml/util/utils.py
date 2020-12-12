7bbae19e777ead8b3b5e5edccefce9adb8d7b9cc
grilhami <gilangrilhami@hotmail.co.uk>
2020-12-12 11:16:31 +0000

    Added function to copy docstring in ml/util/utils.py

--- pydp/ml/util/utils.py
+++ pydp/ml/util/utils.py
@@ -15,3 +15,22 @@ def global_seed(seed):
     None
     """
     np.random.seed(seed)
+
+def copy_docstring(source):
+    """Decorator function to copy a docstring from a `source` function to a `target` function.
+    The docstring is only copied if a docstring is present in `source`, and if none is present in `target`.  Takes
+    inspiration from similar in `matplotlib`.
+    Parameters
+    ----------
+    source : method
+        Source function from which to copy the docstring.  If ``source.__doc__`` is empty, do nothing.
+    Returns
+    -------
+    target : method
+        Target function with new docstring.
+    """
+    def copy_func(target):
+        if source.__doc__ and not target.__doc__:
+            target.__doc__ = source.__doc__
+        return target
+    return copy_func

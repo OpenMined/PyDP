d5b4d691e4dca7ec8df26ad825d55d6921f31cd1
Prachi-Agr <f20180716@pilani.bits-pilani.ac.in>
2021-09-24 01:14:33 +0530

    sklearn pipeline tests

--- tests/sklearn_ml/test_sklearnpipeline.py
+++ tests/sklearn_ml/test_sklearnpipeline.py
@@ -4,23 +4,26 @@ import numbers
 import numpy as np
 
 class TestLocalDP:
-    self.laplace_object=LaplaceMechanism()
     def test_epsilon_type(self):
-        assert isinstance(self.laplace_object.epsilon,numbers.Number)
+        assert isinstance(LaplaceMechanism().epsilon,numbers.Number)
     def test_epsilon_val(self):
-        assert self.laplace_object.epsilon > 0
+        assert LaplaceMechanism().epsilon > 0
     
     def test_sensitivity(self):
-        if(isinstance(self.laplace_object.sensitivity, numbers.Number)):
-            assert self.laplace_object.sensitivity > 0
+        if(isinstance(LaplaceMechanism().sensitivity, numbers.Number)):
+            assert LaplaceMechanism().sensitivity > 0
 
-    @pytest.mark.parametrize("array", [np.array([1,2,3,4]), np.array([[1,2,4,5],[7,8,9,10]])])
+    @pytest.mark.parametrize("array", [np.array([[1,2,4,5],[7,8,9,10]])])
     def test_sensitivity_calculation(self,array):
-        noisy_array=self.laplace_object.sensitivity_calculation(array)
-        assert isinstance(noisy_array,np.array)
+        sensitivity_example=sensitivity_callable()
+        noisy_array=LaplaceMechanism(epsilon=1,sensitivity=sensitivity_example).sensitivity_calculation(array)
+        assert isinstance(noisy_array,np.ndarray)
         assert array.shape==noisy_array.shape
         assert noisy_array.ndim==2
     
+class sensitivity_callable:
+    def __call__(self,x):
+        return abs(x[0]-0.1)
 
 
 

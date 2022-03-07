e58e192d8bd663ba06b143ea3ec3de390fd46e7f
grilhami <gilangrilhami@hotmail.co.uk>
2022-03-07 05:11:56 +0000

    FEAT: SKLearn Laplace Mechanism use Google DP
    Modifie the `LaplaceMechanism`class in
    `/src/pydp/ml/mechanisms/sklearn_pipeline.py`
    so that it uses `LaplaceDistribution` class
    from Google DP`

--- src/pydp/ml/mechanisms/sklearn_pipeline.py
+++ src/pydp/ml/mechanisms/sklearn_pipeline.py
@@ -1,7 +1,6 @@
 import numbers
 
-from ..util.accountant import BudgetAccountant
-from .laplace import Laplace
+from pydp.distributions import LaplaceDistribution  # type: ignore
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -14,7 +13,7 @@ class LaplaceMechanism(BaseEstimator, TransformerMixin):
         Paper link: https://link.springer.com/content/pdf/10.1007/11681878_14.pdf
     """
     
-    def __init__(self, epsilon=1.0, sensitivity=1, accountant=None):
+    def __init__(self, epsilon=1.0, sensitivity=1, scale=None, cat_feat_idxs=None, cat_sensitivity=None):
         
         """
         Checks that all parameters of the mechanism have been initialised correctly, and that the mechanism is ready
@@ -29,8 +28,13 @@ class LaplaceMechanism(BaseEstimator, TransformerMixin):
         sensitivity : float or int
             The sensitivity of the mechanism.  Must satisfy `sensitivity` > 0.
             
-        accountant : BudgetAccountant, optional
-            Accountant to keep track of privacy budget.
+        scale : float or int, optional
+            
+        cat_feat_idxs : list or None, optional
+            List of integers identifying indicies of categorical features.
+            
+        cat_feat_idxs : list or None, optional
+            List of integers identifying indicies of categorical features.
             
             
         Attributes
@@ -41,9 +45,15 @@ class LaplaceMechanism(BaseEstimator, TransformerMixin):
         sensitivty
             Sensitivity of the mechanism to calculate noise.
         
-        accountant
-            Accountant to keep track of privacy budget.
-        
+        scale
+            
+            
+        cat_feat_idxs
+            List of indicies that identifies categorical features.
+            
+        cat_sensitivty
+            Sensitivity of the mechanism to calculate noise for categorical data.
+            
         Raises
         ------
         TypeError
@@ -70,12 +80,32 @@ class LaplaceMechanism(BaseEstimator, TransformerMixin):
             raise ValueError("Sensitivity must be at least larger than 0.")
         
         self.sensitivity = sensitivity
-        self.accountant = BudgetAccountant.load_default(accountant)
         
-        self.laplace = None # If sensitivity is callable, set lapalace to None
+        if scale is not None:
+            if not isinstance(scale, numbers.Number):
+                raise TypeError(f"Sensitivity  must be a int or float. Got type {type(sensitivity)}.")
         
-        if not callable(sensitivity):
-                self.laplace = Laplace()
+        self.scale = scale
+        
+        if (cat_feat_idxs is not None and cat_sensitivity is None) or (cat_feat_idxs is None and cat_sensitivity is not None):
+            raise ValueError("cat_feat_idxs cannot be None if cat_sensitivity, and vice versa.")
+                
+        self.categorical_exists = cat_feat_idxs is not None and cat_sensitivity is not None
+            
+        if self.categorical_exists:
+            
+            if not isinstance(sensitivity, numbers.Number):
+                if not callable(sensitivity):
+                    raise TypeError(f"Sensitivity  must be a number or callable. Got type {type(sensitivity)}.")
+                    
+            if isinstance(sensitivity, numbers.Number) and sensitivity <= 0:
+                raise ValueError("Sensitivity must be at least larger than 0.")
+                
+            if len(cat_feat_idxs) == 0:
+                raise ValueError("At least 1 categorical feature index must be provided.")
+                
+        self.cat_feat_idxs = cat_feat_idxs
+        self.cat_sensitivity = cat_sensitivity
     
     def sensitivity_calculation(self, X):
         """
@@ -96,40 +126,44 @@ class LaplaceMechanism(BaseEstimator, TransformerMixin):
         
         n_feature = X.shape[-1]
         n_data = X.shape[0]
-
         
         for data_idx in range(n_data):
-            self.accountant.check(self.epsilon, 0)
             for feature_idx in range(n_feature):
                 
                 # Array with data point data_idx removed for feature_idx
                 feature = np.concatenate((X[:data_idx,feature_idx],X[data_idx + 1:,feature_idx]))
                 
                 # Calculate sensitivity
-                sensitivity_ = self.sensitivity(feature)
-                
+                if  self.categorical_exists and feature_idx in cat_feat_idxs:
+                    if isinstance(self.cat_sensitivity, numbers.Number):
+                        sensitivity_ = self.cat_sensitivity
+                        print(sensitivity_)
+                    else:
+                        sensitivity_ = self.cat_sensitivity(feature)
+                        print(sensitivity_)
+                        
+                else:
+                    if isinstance(self.sensitivity, numbers.Number):
+                        sensitivity_ = self.sensitivity
+                    else:
+                        sensitivity_ = self.sensitivity(feature)
+                        
                 # Initialized Laplace mechanism instance
-                laplace = Laplace().set_epsilon(self.epsilon).set_sensitivity(sensitivity_)
+                laplace = LaplaceDistribution(epsilon=float(self.epsilon), sensitivity=float(sensitivity_))
                 
-                # Add noise to the data point that was removed
-                noised_value = laplace.randomise(X[data_idx,feature_idx])
+                # Add noise to the data point that was removed 
+                if self.scale is not None:
+                    noised_value = X[data_idx,feature_idx] - laplace.sample(scale=float(self.scale))
+                else:
+                    noised_value = X[data_idx,feature_idx] - laplace.sample()
                 
                 # Replaced data point in the dataset with noised version
                 X[data_idx,feature_idx] = noised_value
-                
-                self.accountant.spend(self.epsilon, 0)
         return X
-    
     
     def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
-        if self.laplace is not None:
-            self.laplace.set_epsilon(self.epsilon).set_sensitivity(self.sensitivity)
-            vector_randomise = np.vectorize(self.laplace.randomise)
-            noised_array = vector_randomise(X)
-            return noised_array
-        else:
-            X = self.sensitivity_calculation( X)
-            return X
+        X = self.sensitivity_calculation( X)
+        return X

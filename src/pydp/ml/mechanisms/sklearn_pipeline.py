02e2850dcc63a83d28e1402779bbbb77f9d89806
SandeepJabez <sandeepjabez@yahoo.com>
2022-07-04 13:11:15 +0530

    gaussianmechanism

--- src/pydp/ml/mechanisms/sklearn_pipeline.py
+++ src/pydp/ml/mechanisms/sklearn_pipeline.py
@@ -7,7 +7,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 import numpy as np
 
-class LaplaceMechanism(BaseEstimator, TransformerMixin):
+class LaplaceMechanism (BaseEstimator, TransformerMixin):
     """
         An SKLearn Pipeline operator for applying differentially private noise 
         addition using the laplace mechanism.
@@ -133,3 +133,138 @@ class LaplaceMechanism(BaseEstimator, TransformerMixin):
         else:
             X = self.sensitivity_calculation( X)
             return X
+
+
+
+
+
+# for gaussian
+class GaussianMechanism(BaseEstimator, TransformerMixin):
+    """
+        An SKLearn Pipeline operator for applying differentially private noise 
+        addition using the gaussian mechanism.
+        
+    """
+    
+    def __init__(self, delta=1.0, sensitivity=1, accountant=None):
+            
+            """
+            Checks that all parameters of the mechanism have been initialised correctly, and that the mechanism is ready
+            to be used.
+            
+            Parameters
+            ----------
+            delta : float or int
+                The value of delta for achieving :math:`\delta`-differential privacy with the mechanism.  Must have
+                `delta > 0`.
+            
+            sensitivity : float or int
+                The sensitivity of the mechanism.  Must satisfy `sensitivity` > 0.
+                
+            accountant : BudgetAccountant, optional
+                Accountant to keep track of privacy budget.
+                
+                
+            Attributes
+            -------
+            delta
+                Privacy budget to calculate noise.
+                
+            sensitivty
+                Sensitivity of the mechanism to calculate noise.
+            
+            accountant
+                Accountant to keep track of privacy budget.
+            
+            Raises
+            ------
+            TypeError
+                If delta is not a number, or sensitivity is not a number or a callable.
+                
+            ValueError
+                If delta less than 0, or sensitivty is a number but less than 0.
+            """
+            
+            if not isinstance(delta, numbers.Number):
+                raise TypeError(f"Delta  must be a number. Got type {type(delta)}.")
+    
+            if delta <= 0:
+                raise ValueError("Delta must be at least larger than 0.")
+                
+            self.delta = delta
+            
+            if not isinstance(sensitivity, numbers.Number):
+                if not callable(sensitivity):
+                    raise TypeError(f"Sensitivity  must be a number or callable. Got type {type(sensitivity)}.")
+    
+                
+            if isinstance(sensitivity, numbers.Number) and sensitivity <= 0:
+                raise ValueError("Sensitivity must be at least larger than 0.")
+            
+            self.sensitivity = sensitivity
+            self.accountant = BudgetAccountant.load_default(accountant)
+            
+            self.gaussian = None
+
+            if not callable(sensitivity):
+                self.gaussian = Gaussian()
+                
+    def sensitivity_calculation(self, X):
+        """
+        Perform local differential privacy by adding noise using Laplace mechanismto the dataset if the sensitivity 
+        provided if a callable.
+        
+        
+        Parameters
+        ----------
+        X : numpy.array
+            Datset in the form of a 2-dimensional numpy array.
+            
+        Returns
+        ------
+        X : numpy.array
+            Original parameter X with differentially private noise added.
+        """
+        
+        n_feature = X.shape[-1]
+        n_data = X.shape[0]
+
+        
+        for data_idx in range(n_data):
+            self.accountant.check(self.delta, 0)
+            for feature_idx in range(n_feature):
+                
+                # Array with data point data_idx removed for feature_idx
+                feature = np.concatenate((X[:data_idx,feature_idx],X[data_idx + 1:,feature_idx]))
+                
+                # Calculate sensitivity
+                sensitivity_ = self.sensitivity(feature)
+                
+                # Initialized Gaussian mechanism instance
+                gaussian = Gaussian().set_delta(self.delta).set_sensitivity(sensitivity_)
+                
+                # Add noise to the data point that was removed
+                noised_value = gaussian.randomise(X[data_idx,feature_idx])
+                
+                # Replaced data point in the dataset with noised version
+                X[data_idx,feature_idx] = noised_value
+                
+                self.accountant.spend(self.delta, 0)
+        return X
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        if self.gaussian is not None:
+            self.gaussian.set_delta(self.delta).set_sensitivity(self.sensitivity)
+            vector_randomise = np.vectorize(self.gaussian.randomise)
+            noised_array = vector_randomise(X)
+            return noised_array
+        else:
+            X = self.sensitivity_calculation( X)
+            return X
+
+            
+
+    

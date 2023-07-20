73471bbc0367e8f59ed5312bdcc54b58cef7c9f6
chinmay <chinmayshah3899@gmail.com>
2023-07-20 10:25:37 +0530

    updated package version for launch

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     python_requires=">=3.8",
     test_suite="tests",
     url="https://github.com/OpenMined/PyDP",
-    version="1.1.3rc6",
+    version="1.1.4rc1",
     zip_safe=False,
 )

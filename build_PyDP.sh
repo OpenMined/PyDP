b896a3a3b379841a7336841a192ab3efb8b42f00
Vadym Doroshenko <dvadym@google.com>
2025-08-24 15:02:28 +0200

    debug

--- build_PyDP.sh
+++ build_PyDP.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e # exit on error
 
+echo "*** Build start"
+pwd
+ls -l
+
 mkdir -p build
 cd build 
+pwd
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make _pydp -j 
 mv ./src/bindings/PyDP/_pydp*so ../src/pydp/_pydp.so 

6aefe0ff559476176df5eb3e1c06e233e883fc4a
Vadym Doroshenko <dvadym@google.com>
2025-08-25 09:09:39 +0200

    fix

--- build_PyDP.sh
+++ build_PyDP.sh
@@ -3,6 +3,6 @@ set -e # exit on error
 
 mkdir -p build
 cd build 
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=arm64
 make _pydp -j 
 mv ./src/bindings/PyDP/_pydp*so ../src/pydp/_pydp.so 

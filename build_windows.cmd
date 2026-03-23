a2da87956324e18b95b502e5a7c0936008e725d3
Vadym Doroshenko <dvadym@google.com>
2026-03-23 17:15:54 +0100

    debug4

--- build_windows.cmd
+++ build_windows.cmd
@@ -1,4 +1,4 @@
 mkdir build_dir
 cd build_dir
-cmake -S .. -B .  -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL" -DOPENSSL_USE_STATIC_LIBS=TRUE
+cmake -S .. -B .  -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL"
 cmake --build . --parallel --config Release

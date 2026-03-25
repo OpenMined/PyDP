f423426b841c5789661b786dc1d75567993b37ec
Vadym Doroshenko <dvadym@google.com>
2026-03-25 10:35:23 +0100

    debug17

--- build_windows.cmd
+++ build_windows.cmd
@@ -1,3 +1,6 @@
+git clone https://github.com/Delphier/dumpbin.git
+python dumpbin/dumpbin.py
+
 mkdir build_dir
 cd build_dir
 cmake -S .. -B . -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL"
@@ -6,7 +9,7 @@ cmake --build . --parallel --config Release
 move "src\bindings\PyDP\Release\_pydp.*-win_amd64.pyd" "..\src\pydp\"
 cd ..\src\pydp\
 ren "_pydp.*-win_amd64.pyd" "_pydp.pyd"
-dumpbin /dependents "_pydp.pyd"
+dumpbin.exe /dependents "_pydp.pyd"
 dir "C:\Program Files\OpenSSL"
 dir "C:\Program Files\OpenSSL\bin"
 copy "C:\Program Files\OpenSSL\libcrypto*.dll" .
